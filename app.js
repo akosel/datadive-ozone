b8f33c90ac65ff5fd35d58b71e211b22e2c55f66
Aaron Kosel <a_kosel@yahoo.com>
2014-11-11 10:08:03 -0500

    minor style changes. added server logging. deleted some cruft

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 var path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(morgan('combined'));
 
 app.get('/', function(req, res) {
   res.render('dashboard.html');
 });
 
 app.get('/calendar', function(req, res) {
-  res.render('index.html');
+  res.render('calendar.html');
 });
 
 var server = app.listen(process.env.PORT || 8000, function() {
